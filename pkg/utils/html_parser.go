1b792f858d2d615a9bcb5a65c61c8f781b8cbcfa
n.ozgurozturk <ozgur@nozgurozturk.com>
2020-11-17 01:18:46 +0300

    feat(pkg) : add html file parser

---
+++ pkg/utils/html_parser.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"bytes"
+	"html/template"
+)
+
+// Parses HTML file to string with given data
+func ParseHTMLTemplate(templateFileName string, data interface{}) (string, error) {
+	t, err := template.ParseFiles(templateFileName)
+	if err != nil {
+		return "", err
+	}
+	buf := new(bytes.Buffer)
+	if err = t.Execute(buf, data); err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}

a65182e02cb30b7ece0e4632dbaa6ac7e01facd8
n.ozgurozturk <ozgur@nozgurozturk.com>
2020-11-17 01:26:22 +0300

    feat(pkg) : add npm into managers

---
+++ pkg/managers/npm.go
@@ -0,0 +1,27 @@
+package managers
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/nozguozturk/marvin/client"
+)
+
+type Npm struct {
+	apiUrl string
+}
+
+func (n *Npm) GetRegistryVersion(registryName string) (string, error) {
+
+	endpoint := fmt.Sprintf("/%s", registryName)
+	registryData, err := client.New(n.apiUrl).Get(endpoint, nil)
+	if err != nil {
+		return "", err
+	}
+	var registry map[string]interface{}
+	if err := json.Unmarshal(registryData, &registry); err != nil {
+		return "", err
+	}
+	registryVersion := registry["dist-tags"].(map[string]interface{})["latest"].(string)
+	return registryVersion, nil
+}
+
